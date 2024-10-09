452c47ef5618a6e435408f196f3276a9dad82235
IngLuisNavas <ingluisnavas@gmail.com>
2024-10-09 00:22:11 -0500

    Añadir validación de datos en validacion.php

--- validacion.php
+++ validacion.php
@@ -17,25 +17,25 @@ if ($conexion->connect_error) {
 }
 
 // Verificar si las variables están definidas
-if (!isset($_POST['email_docente'], $_POST['nombre_grupo'], $_POST['jornada'], $_POST['dia_semana'], $_POST['hora'], $_POST['espacio'], $_POST['recurso'])) {
+if (!isset($_POST['email_docente'], $_POST['nombre_grupo'], $_POST['fecha_reserva'], $_POST['hora_inicio'], $_POST['hora_fin'], $_POST['espacio'], $_POST['recurso'])) {
     die("Error: Faltan datos en el formulario.");
 }
 
 $email_docente = $_POST['email_docente'];
 $nombre_grupo = $_POST['nombre_grupo'];
-$jornada = $_POST['jornada'];
-$dia_semana = $_POST['dia_semana'];
-$hora = $_POST['hora'];
+$fecha_reserva = $_POST['fecha_reserva'];
+$hora_inicio = $_POST['hora_inicio'];
+$hora_fin = $_POST['hora_fin'];
 $espacio = $_POST['espacio'];
 $recurso = $_POST['recurso'];
 
 // Insertar nueva reserva
-$sql_insertar_reserva = "INSERT INTO Reservas (email_docente, nombre_grupo, jornada, dia_semana, hora, EspacioID, RecursoID) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$sql_insertar_reserva = "INSERT INTO Reservas (email_docente, nombre_grupo, fecha_reserva, hora_inicio, hora_fin, Espacio, Recurso) VALUES (?, ?, ?, ?, ?, ?, ?)";
 $stmt_insertar_reserva = $conexion->prepare($sql_insertar_reserva);
 if (!$stmt_insertar_reserva) {
     die("Error en la preparación de la consulta: " . $conexion->error);
 }
-$stmt_insertar_reserva->bind_param("sssssss", $email_docente, $nombre_grupo, $jornada, $dia_semana, $hora, $espacio, $recurso);
+$stmt_insertar_reserva->bind_param("sssssss", $email_docente, $nombre_grupo, $fecha_reserva, $hora_inicio, $hora_fin, $espacio, $recurso);
 if ($stmt_insertar_reserva->execute()) {
     echo "Reserva creada con éxito.";
 } else {
